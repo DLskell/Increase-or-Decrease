5e3f6785fed807592bea11aef0cecf0dfa7c6234
Loukastor <156593512+DLskell@users.noreply.github.com>
2024-01-27 10:11:37 +0100

    Update Increase or Decrease.py

--- Increase or Decrease.py
+++ Increase or Decrease.py
@@ -1,4 +1,4 @@
-#generally useful for sales, but also for other occasions, it can be used to increase or decrease a given amount by a percentage determined by you
+#generally useful for sales, but also for other uses, it can be used to increase or decrease a given amount by a pre-determined percentage
 a=3
 while a!=1 and a!=2 and a!=0:
     print("Type 1 for an increase\nType 2 for a decrease\nType 0 to cancel")
@@ -8,10 +8,10 @@ if a==1:
     b=float(input("The data to increase : "))
     c=float(input("The percentage increase : "))
     b=b+b*c/100
-    print("After augmentation, we obtain ",b)
+    print("The result is : ",b)
 
 if a==2:
     b=float(input("The data to reduce : "))
-    c=float(input("The percentage reduction : "))
+    c=float(input("The reduction percentage : "))
     b=b-(b*c/100)
-    print("After reduction, we obtain ",b)
+    print("The result is : ",b)
